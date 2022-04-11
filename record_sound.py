c8cc125efd51fdc114b82dbf86c570af77729658
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-11 12:47:21 +0200

    increased chunk to 48000

--- record_sound.py
+++ record_sound.py
@@ -5,7 +5,7 @@ def record():
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
-    CHUNK = 4096
+    CHUNK = 4800
     RECORD_SECONDS = 5
     WAVE_OUTPUT_FILENAME = "file.wav"
     
