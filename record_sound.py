b58ed2056d652049e5245356717705a2af4047ce
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-11 12:55:43 +0200

    increased sample rate  to 48000

--- record_sound.py
+++ record_sound.py
@@ -4,7 +4,7 @@ import wave
 def record():
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
-    RATE = 44100
+    RATE = 48000
     CHUNK = 48000
     RECORD_SECONDS = 5
     WAVE_OUTPUT_FILENAME = "file.wav"
