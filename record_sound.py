be000b5c754a6e4973dd1bc04af48ba9efb0ba5f
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-11 12:48:59 +0200

    added sample rate

--- record_sound.py
+++ record_sound.py
@@ -40,6 +40,7 @@ def get_device_info():
     p = pyaudio.PyAudio()
     for i in range(p.get_device_count()):
         print(p.get_device_info_by_index(i))
+        print(f"sample rate: {p.get_device_info_by_index(0)['defaultSampleRate']}\n")
 
 def main():
     get_device_info()
