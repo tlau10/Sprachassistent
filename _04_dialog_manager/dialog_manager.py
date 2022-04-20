6cfc160f79c516f0cac9a18a97a519c8f5c38853
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-20 10:27:00 +0200

    replaced coqui tts with pico tts

--- _04_dialog_manager/dialog_manager.py
+++ _04_dialog_manager/dialog_manager.py
@@ -1,8 +1,6 @@
 from voice_assistant_helper import read_json_file
 from decouple import config
-import json
 import wikipediaapi
-import os
 import subprocess
 
 def default():
@@ -13,20 +11,16 @@ def execute_wikipedia_skill(slot_values):
     wikipedia_page = wikipedia.page(slot_values[0])
     page_summary = wikipedia_page.summary
     first_sentence = page_summary.split('.')[0]
-    # TTS hängt sich an Satzzeichen und Umlauten auf -> evtl. anderes TTS auch mit mehr roboter artiger Sprache
-    first_sentence = first_sentence.replace(',', '')
+    first_sentence = '"' + first_sentence + '"'
+
     print(first_sentence)
 
     run_tts(first_sentence)
 
 def run_tts(text):
-    output_file_path = config('DM_OUTPUT_PATH')
-    model_path = config('TTS_MODEL_PATH')
-    os.system('tts'+' --text ' + '"'+ text +'"' + ' --model_name ' + '"' + model_path + '"' + ' --model_name ' + '"' + model_path + '"')
-    # TODO: os.system mit subprocess.run ersetzen um injections zu verhindern
-
-def run_tts_2(text):
-    pass
+    output_file_path = config('TTS_OUTPUT_PATH')
+    subprocess.call(['pico2wave', '-l', 'de-DE', '-w', output_file_path, text])
+    subprocess.call(['aplay', output_file_path])
 
 
 intent = read_json_file(config('NLU_OUTPUT_PATH'))
