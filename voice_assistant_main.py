fa7b046c77b5eb586bf0e367feafc382e489259b
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-20 11:29:53 +0200

    replaced os.system with subprocess

--- voice_assistant_main.py
+++ voice_assistant_main.py
@@ -1,17 +1,15 @@
 from _01_wake_word_detection.wake_word_detection import WakeWordDetection
 from _03_natural_language_understanding.natural_language_understanding import NLU
 from _04_dialog_manager.dialog_manager import DialogManager
-import os
+import subprocess
+from decouple import config
 
 wake_word_detection = WakeWordDetection()
 wake_word_detection.start()
 
-# TODO replace with subprocess.call
-# TODO write paths to .env
-os.system('python3 _02_speech_to_text/mic_vad_streaming.py\
-    --model _02_speech_to_text/model.pbmm\
-    --scorer _02_speech_to_text/de-aashishag-1-prune-kenlm.scorer\
-    --savewav _02_speech_to_text/stt_audio')
+subprocess.check_call(['python3', '_02_speech_to_text/mic_vad_streaming.py',
+'--model', config('STT_MODEL_PATH'), '--scorer', config('STT_SCORER_PATH'),
+'--savewav', config('STT_WAV_OUTPUT_PATH')])
 
 nlu = NLU()
 nlu.train()
@@ -19,6 +17,3 @@ nlu.parse()
 
 dialog_manager = DialogManager()
 dialog_manager.start()
-
-# clean up
-os.system('rm -r _02_speech_to_text/stt_audio/*.wav')
