364aac624a63b5ca77415b473fba7b691504ecfa
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-04-25 17:35:06 +0200

    fixed listeners module not found

--- _04_dialog_manager/observer.py
+++ _04_dialog_manager/observer.py
@@ -1,7 +1,7 @@
-from listeners.none_skill_listener import setup_none_event_handlers
-from listeners.text_to_speech_listener import setup_text_to_speech_event_handlers
-from listeners.stop_skill_listener import setup_stop_event_handlers
-from listeners.wikipedia_skill_listener import setup_wikipedia_event_handlers
+from _04_dialog_manager.listeners.text_to_speech_listener import setup_text_to_speech_event_handlers
+from _04_dialog_manager.listeners.stop_skill_listener import setup_stop_event_handlers
+from _04_dialog_manager.listeners.wikipedia_skill_listener import setup_wikipedia_event_handlers
+from _04_dialog_manager.listeners.none_skill_listener import setup_none_event_handlers
 
 def setup_event_handlers():
     """
