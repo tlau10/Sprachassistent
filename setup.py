8d61eed368c4de41313f389aba5d402681b2451f
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-05-13 10:10:39 +0200

    changed cron job setup

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ COMMAND = "python _04_dialog_manager/mensa_parser/mensa_data_requester.py"
 
 crontab_ = CronTab(user = USER)
 
-print("setting up cron job...")
+if crontab_.find_command('mensa_data_requester.py'):
+    print("cron job already exists")
+else:
+    print("setting up cron job...")
 
-cron_job = crontab_.new(command = COMMAND)
-cron_job.day.on(6)
-crontab_.write()
+    cron_job = crontab_.new(command = COMMAND)
+    cron_job.day.on(6)
+    crontab_.write()
 
-print("cronjob successfully set up")
+    print("cron job successfully set up")
 
 # start main.py
 subprocess.run(['python', 'voice_assistant_main.py'])
