287f9bb8812e9994fe2ca91efa61e1df90825512
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-05-13 10:14:45 +0200

    added removal of all cron jobs for user

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import subprocess
 from crontab import CronTab
 
-# setup and shedule cronjob to receive latest mensa data
+# removes all cron jobs then setups and shedules cronjob to receive latest mensa data
 USER = "pi"
 COMMAND = "python _04_dialog_manager/mensa_parser/mensa_data_requester.py"
 
 crontab_ = CronTab(user = USER)
 
-if crontab_.find_command(COMMAND):
-    print("cron job already exists")
-else:
-    print("setting up cron job...")
+print(f"removing all cron job for user {USER}...")
+crontab_.remove_all()
 
-    cron_job = crontab_.new(command = COMMAND)
-    cron_job.day.on(6)
-    crontab_.write()
+print("setting up cron job...")
 
-    print("cron job successfully set up")
+cron_job = crontab_.new(command = COMMAND)
+cron_job.day.on(6)
+crontab_.write()
+
+print("cron job successfully set up")
 
 # start main.py
 subprocess.run(['python', 'voice_assistant_main.py'])
