9416bd589a8980b33801f4e833a6632308354753
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-05-23 15:50:29 +0200

    added more dummy intents

--- _04_dialog_manager/listeners/dummy_skill_listener.py
+++ _04_dialog_manager/listeners/dummy_skill_listener.py
@@ -3,9 +3,25 @@ from _04_dialog_manager.event import subscribe, post_event
 def handle_dummy_event_a(data):
     """
     dummy event for scalability test,
-    posts dialog_manager_output event
+    uses helper method to post dialog_manager_output event
     """
-    response = f"Dummy event a erkannt"
+    post_event_dm_output("Dummy event a erkannt")
+
+def handle_dummy_event_b(data):
+    """
+    dummy event for scalability test,
+    uses helper method to post dialog_manager_output event
+    """
+    post_event_dm_output("Dummy event b erkannt")
+
+def handle_dummy_event_c(data):
+    """
+    dummy event for scalability test,
+    uses helper method to post dialog_manager_output event
+    """
+    post_event_dm_output("Dummy event c erkannt")
+
+def post_event_dm_output(response):
     post_event("dialog_manager_output", response)
 
 def setup_dummy_event_handler():
@@ -13,3 +29,5 @@ def setup_dummy_event_handler():
     subcribes all events 
     """
     subscribe("dummy_a", handle_dummy_event_a)
+    subscribe("dummy_b", handle_dummy_event_b)
+    subscribe("dummy_c", handle_dummy_event_c)
