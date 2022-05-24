4247663b6e427e04e3da8d70f0ab51444f0d305c
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-05-24 10:29:09 +0200

    changed setup_cronjob script and readme

--- setup_cronjob.py
+++ setup_cronjob.py
@@ -1,7 +1,8 @@
 from crontab import CronTab
+import os
 
 # setups and shedules cronjob to receive latest mensa data
-USER = "pi"
+USER = os.getlogin()
 COMMAND = "python _04_dialog_manager/mensa_parser/mensa_data_requester.py"
 
 crontab_ = CronTab(user = USER)
@@ -9,7 +10,7 @@ crontab_ = CronTab(user = USER)
 print(f"setting up cron job for user {USER}...")
 
 cron_job = crontab_.new(command = COMMAND)
-cron_job.setall('0 0 * * 0')
+cron_job.setall('0 6 * * 1')
 crontab_.write()
 
-print("cron job successfully set up")
+print(f"cron job successfully set up for user {USER}")
