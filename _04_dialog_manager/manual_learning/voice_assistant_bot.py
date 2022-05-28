87c52ee4755c50dfa9dc8b05125ea203e68c20e2
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-05-28 19:34:45 +0200

    renewed entries.json and finalized store_entry function

--- _04_dialog_manager/manual_learning/voice_assistant_bot.py
+++ _04_dialog_manager/manual_learning/voice_assistant_bot.py
@@ -1,9 +1,9 @@
-from genericpath import exists
 import telebot
-from voice_assistant_helper import read_from_file
+from voice_assistant_helper import read_from_file, read_json_file, write_json_file
 from decouple import config
 
 API_KEY = read_from_file(file_path = config('TELEGRAM_BOT_KEY'))
+JSON_FILE_OUTPUT_PATH = config('BOT_ENTRIES')
 
 bot = telebot.TeleBot(API_KEY)
 registered_user_id = list()
@@ -53,7 +53,8 @@ def unsubscribe(message):
 def improve(message):
     chat_id = message.chat.id
 
-    if message.reply_to_message is not None:
+    # check if its a reply to a message sent by the bot
+    if message.reply_to_message is not None and message.reply_to_message.from_user.is_bot is True:
         replied_message = message.reply_to_message.text
 
         bot.send_message(chat_id, f"Succesfully improved {replied_message}!")
@@ -61,21 +62,28 @@ def improve(message):
         intent, slot, recognized_value = replied_message.split(": ")
         improved_value = message.text.replace("/improve ", "")
 
-        write_entry((intent, slot, recognized_value, improved_value))
+        store_entry((intent, slot, recognized_value, improved_value))
     else:
-        bot.send_message(chat_id, "Can only be used in combination with a reply message")
+        bot.send_message(chat_id, f"Use by replying to a message sent by the voice assistant bot then type /{commands[3]} followed by your improvement")
 
-def write_entry(entry):
+def store_entry(entry):
     """
-    writes new entry to file
+    writes new entry to json file
     @param entry: entry to write to file as tuple(intent, slot, replied_message, improved_message)
     """
     intent, slot, recognized_value, improved_value = entry
     print(f"intent: {intent}, slot: {slot}, recognized_value: {recognized_value}, improved_value: {improved_value}")
-    # read json file
-    # convert to dict
-    # append value
-    # convert to json
-    # store in json file
+
+    # read json
+    entries = read_json_file(file_path = JSON_FILE_OUTPUT_PATH)
+
+    # retrieve entry
+    entry = entries[intent][slot]
+
+    # append new key value pair
+    kv = {recognized_value : improved_value}
+    entry['values'] = kv
+
+    write_json_file(file_path = JSON_FILE_OUTPUT_PATH, json_object = entries)
 
 bot.polling()
