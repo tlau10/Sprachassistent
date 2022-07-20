a18b779a53e7dfed547c2c7d4672df86dce2baac
Tim Laukemann <ti391lau@htwg-konstanz.de>
2022-07-20 14:08:45 +0200

    refactored speecht_to_text.py

--- _02_speech_to_text/speech_to_text.py
+++ _02_speech_to_text/speech_to_text.py
@@ -1,19 +1,21 @@
 """
 Partly taken from Mozilla DeepSpeech
-GitHub repository: https://github.com/mozilla/DeepSpeech-examples/blob/r0.9/mic_vad_streaming/README.rst
+GitHub repository: 
+https://github.com/mozilla/DeepSpeech-examples/blob/r0.9/mic_vad_streaming/README.rst
 """
 import logging
-import collections, queue, os, os.path
+import collections
+import queue
+import platform
+import wave
 import deepspeech
 import numpy as np
 import pyaudio
-import wave
 import webrtcvad
 from halo import Halo
 from scipy import signal
 from decouple import config
 from voice_assistant_helper import write_to_file
-import platform
 
 logging.basicConfig(level=20)
 
@@ -30,8 +32,8 @@ class SpeechToText:
         text_output_path = config('STT_OUTPUT_PATH')
 
         self.deep_speech = DeepSpeech(
-            model_path = model_path, 
-            scorer_path = scorer_path, 
+            model_path = model_path,
+            scorer_path = scorer_path,
             text_output_path = text_output_path)
 
     def start(self):
@@ -46,8 +48,8 @@ class SpeechToText:
 
         # Start audio with VAD
         vad_audio = VADAudio(
-            aggressiveness = 3, 
-            device = None, 
+            aggressiveness = 3,
+            device = None,
             input_rate = rate,
             file = None
         )
@@ -60,14 +62,16 @@ class SpeechToText:
         text_path = self.deep_speech.text_output_path
         for frame in frames:
             if frame is not None:
-                if spinner: spinner.start()
+                if spinner:
+                    spinner.start()
                 logging.debug("streaming frame")
                 stream_context.feedAudioContent(np.frombuffer(frame, np.int16))
             else:
-                if spinner: spinner.stop()
+                if spinner: 
+                    spinner.stop()
                 logging.debug("end utterence")
                 text = stream_context.finishStream()
-                print("speech-to-text recognized: %s" % text)
+                print(f"speech-to-text recognized: {text}")
                 write_to_file(file_path = text_path, text = text)
                 return
 
@@ -83,8 +87,9 @@ class DeepSpeech:
         )
         self.engine.enableExternalScorer(scorer_path = self.scorer_path)
 
-class Audio(object):
-    """Streams raw audio from microphone. Data is received in a separate thread, and stored in a buffer, to be read from."""
+class Audio:
+    """Streams raw audio from microphone. Data is received in a separate thread,
+    and stored in a buffer, to be read from."""
 
     FORMAT = pyaudio.paInt16
     # Network/VAD rate-space
@@ -94,19 +99,19 @@ class Audio(object):
 
     def __init__(self, callback=None, device=None, input_rate=RATE_PROCESS, file=None):
         def proxy_callback(in_data, frame_count, time_info, status):
-            #pylint: disable=unused-argument
             if self.chunk is not None:
-                in_data = self.wf.readframes(self.chunk)
+                in_data = self.wave_file.readframes(self.chunk)
             callback(in_data)
             return (None, pyaudio.paContinue)
-        if callback is None: callback = lambda in_data: self.buffer_queue.put(in_data)
+        if callback is None:
+            callback = lambda in_data: self.buffer_queue.put(in_data)
         self.buffer_queue = queue.Queue()
         self.device = device
         self.input_rate = input_rate
         self.sample_rate = self.RATE_PROCESS
         self.block_size = int(self.RATE_PROCESS / float(self.BLOCKS_PER_SECOND))
         self.block_size_input = int(self.input_rate / float(self.BLOCKS_PER_SECOND))
-        self.pa = pyaudio.PyAudio()
+        self.pyaudio = pyaudio.PyAudio()
 
         kwargs = {
             'format': self.FORMAT,
@@ -123,9 +128,9 @@ class Audio(object):
             kwargs['input_device_index'] = self.device
         elif file is not None:
             self.chunk = 320
-            self.wf = wave.open(file, 'rb')
+            self.wave_file= wave.open(file, 'rb')
 
-        self.stream = self.pa.open(**kwargs)
+        self.stream = self.pyaudio.open(**kwargs)
         self.stream.start_stream()
 
     def resample(self, data, input_rate):
@@ -156,20 +161,19 @@ class Audio(object):
     def destroy(self):
         self.stream.stop_stream()
         self.stream.close()
-        self.pa.terminate()
+        self.pyaudio.terminate()
 
     frame_duration_ms = property(lambda self: 1000 * self.block_size // self.sample_rate)
 
     def write_wav(self, filename, data):
         logging.info("write wav %s", filename)
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(self.CHANNELS)
-        # wf.setsampwidth(self.pa.get_sample_size(FORMAT))
+        wave_file = wave.open(filename, 'wb')
+        wave_file.setnchannels(self.CHANNELS)
         assert self.FORMAT == pyaudio.paInt16
-        wf.setsampwidth(2)
-        wf.setframerate(self.sample_rate)
-        wf.writeframes(data)
-        wf.close()
+        wave_file.setsampwidth(2)
+        wave_file.setframerate(self.sample_rate)
+        wave_file.writeframes(data)
+        wave_file.close()
 
 
 class VADAudio(Audio):
@@ -189,12 +193,15 @@ class VADAudio(Audio):
                 yield self.read_resampled()
 
     def vad_collector(self, padding_ms=300, ratio=0.75, frames=None):
-        """Generator that yields series of consecutive audio frames comprising each utterence, separated by yielding a single None.
-            Determines voice activity by ratio of frames in padding_ms. Uses a buffer to include padding_ms prior to being triggered.
+        """Generator that yields series of consecutive audio frames comprising each utterence, 
+        separated by yielding a single None.
+        Determines voice activity by ratio of frames in padding_ms.
+        Uses a buffer to include padding_ms prior to being triggered.
             Example: (frame, ..., frame, None, frame, ..., frame, None, ...)
                       |---utterence---|        |---utterence---|
         """
-        if frames is None: frames = self.frame_generator()
+        if frames is None:
+            frames = self.frame_generator()
         num_padding_frames = padding_ms // self.frame_duration_ms
         ring_buffer = collections.deque(maxlen=num_padding_frames)
         triggered = False
